561bc03461dbe22864a74031f151c8f5812bcae4
Odeniran Qozeem Banji <muhdqozeem@gmail.com>
2021-03-21 20:10:57 +0100

    fixed user login

--- api/config/database.php
+++ api/config/database.php
@@ -4,7 +4,7 @@ class Database{
  
     // specify your own database credentials
     private $host = "localhost";
-    private $db_name = "api_jwt_db";
+    private $db_name = "api_db";
     private $username = "root";
     private $password = "";
     public $conn;
--- api/login.php
+++ api/login.php
@@ -1,14 +1,14 @@
 <?php
 // required headers
-header("Access-Control-Allow-Origin: http://localhost/rest-api-authentication-example/");
+header("Access-Control-Allow-Origin: http://php-jwt-api.localhost/");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 // files needed to connect to database
-include_once 'api/config/database.php';
-include_once 'api/objects/user.php';
+include_once '../api/config/database.php';
+include_once '../api/objects/user.php';
 
 // get database connection
 $database = new Database();
@@ -25,11 +25,11 @@ $user->email = $data->email;
 $email_exists = $user->emailExists();
 
 // generate json web token
-include_once 'api/config/core.php';
-include_once 'libs/php-jwt-master/src/BeforeValidException.php';
-include_once 'libs/php-jwt-master/src/ExpiredException.php';
-include_once 'libs/php-jwt-master/src/SignatureInvalidException.php';
-include_once 'libs/php-jwt-master/src/JWT.php';
+include_once '../api/config/core.php';
+include_once '../libs/php-jwt-master/src/BeforeValidException.php';
+include_once '../libs/php-jwt-master/src/ExpiredException.php';
+include_once '../libs/php-jwt-master/src/SignatureInvalidException.php';
+include_once '../libs/php-jwt-master/src/JWT.php';
 use \Firebase\JWT\JWT;
 
 // check if email exists and if password is correct 
